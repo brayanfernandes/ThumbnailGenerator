04e83683ef16b6d952ec3c4e855a68efb5b85b3d
Brayan Fernandes <brayanfernandesmates@gmail.com>
2019-08-25 22:30:06 -0300

    Add files via upload

---
+++ thumbnail_generator/thumbnail.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+import time
+VIDEO_PATH = "C:\\xampp\\htdocs\\videos\\vids\\"
+IMG_PATH = "C:\\xampp\\htdocs\\videos\\thumbnail\\"
+
+files = os.listdir(VIDEO_PATH)
+
+for file in files:
+    video_input_path = VIDEO_PATH + file
+    img_output_path = IMG_PATH + (os.path.splitext(file)[0]) + ".jpg"
+    subprocess.call(['ffmpeg', '-y', '-i', video_input_path, '-ss', '00:00:04.000','-vframes', '1',img_output_path])
+
+
